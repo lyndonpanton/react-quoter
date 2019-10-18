39b421787e66c08b22dfc48130ffd4b2111631af
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-18 18:24:34 +0100

    Updates

---
+++ precache-manifest.4b282e6262783f41f8ad088fd7e6cf7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdef57f7c9c45411a337e72506702096",
+    "url": "/react-quoter/index.html"
+  },
+  {
+    "revision": "9dbdde8ee952ff2fb6d2",
+    "url": "/react-quoter/static/css/main.a4d95488.chunk.css"
+  },
+  {
+    "revision": "09ce76563441566bc5cf",
+    "url": "/react-quoter/static/js/2.1073a5a1.chunk.js"
+  },
+  {
+    "revision": "9dbdde8ee952ff2fb6d2",
+    "url": "/react-quoter/static/js/main.a1cdf680.chunk.js"
+  },
+  {
+    "revision": "9c7bce23427fdc07a7e0",
+    "url": "/react-quoter/static/js/runtime-main.e1a4102a.js"
+  }
+]);
