fb44a8aca8420b6b4dd26adf754043b1ee8e6c38
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-18 15:53:31 +0100

    created component template, exported component

---
+++ src/components/Quote.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Quote = (props) => {
+	return (
+		<blockquote>
+			<q>&ldquo;[Quote here]&rdquo;</q>
+			<cite>[Person here]</cite>
+		</blockquote>
+	);
+};
+
+export default Quote;
