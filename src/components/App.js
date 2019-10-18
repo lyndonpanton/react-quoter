f3679150deffac809a635c7b403defae54c23675
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-18 15:32:01 +0100

    added proerties to component state

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,16 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			author: "Lyndon Panton",
+			copyright: "All Rights Reserved",
+			date: (new Date()).getFullYear(),
+			name: "React Quoter"
+		};
+	};
+
 	render() {
 		return (
 			<div className="app">
