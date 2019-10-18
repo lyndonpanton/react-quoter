d203adec4f6bb62f749d6de9724f355a80681e12
Lyndon Panton <LyndonPanton@gmail.com>
2019-10-18 17:13:39 +0100

    defined, applied styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,10 +13,35 @@ class Footer extends React.Component {
 	render() {
 		const { author, copyright, date } = this.props;
 
+		const footerStyling = {
+			position: "absolute",
+			bottom: "0px",
+
+			width: "100%",
+			backgroundColor: "#2466B4",
+			textAlign: "center",
+			color: "#FFFFFF"
+		};
+
+		const rightsStyling = {
+			opacity: 0.9
+		};
+
+		const creditsStyling = {
+			color: "inherit"
+		};
+
+		const creditStyling = {
+			borderBottom: "#DADADA dotted 1px",
+			textDecoration: "none",
+			fontStyle: "italic",
+			color: "#E5E5E5"
+		};
+
 		return (
-			<footer>
-				<p>&copy; { date } { author } | { copyright }</p>
-				<p>Credit: <a href={this.state.credits.api} target="_blank" rel="noopener noreferrer">JamesFT</a></p>
+			<footer style={footerStyling}>
+				<p style={rightsStyling}>&copy; { date } { author } | { copyright }</p>
+				<p style={creditsStyling}>Credit: <a href={this.state.credits.api} target="_blank" rel="noopener noreferrer" style={creditStyling}>JamesFT</a></p>
 			</footer>
 		);
 	};
